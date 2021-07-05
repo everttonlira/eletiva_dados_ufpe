70119e83ceb48fa076dd0850b269a24d987b63c7
Evertton Lira <46628634+everttonlira@users.noreply.github.com>
2021-07-05 12:29:45 -0300

    script atualizado e comentado

--- scripts/script_avaliacao.R
+++ scripts/script_avaliacao.R
@@ -78,3 +78,55 @@ incidencia <- covid %>% filter(classe == "CONFIRMADO") %>% group_by(municipio) %
 
 # Calculando a letalidade por 100 mil habitantes
 letalidade <- covid %>% filter(evolucao == "OBITO") %>% group_by(municipio) %>% group_by(semana_epi) %>% mutate(letalidade = sum(obitos)/100000)
+
+##################################################################################
+
+## Hugo, tive problemas na questão 3, então tive que enviar da forma que pude. Tentei usar o seguinte script também, 
+# o qual já usei outras vezes, mas também não deu certo
+
+rm_accent <- function(str,pattern="all") {
+  # Rotinas e fun??es ?teis V 1.0
+  # rm.accent - REMOVE ACENTOS DE PALAVRAS
+  # Fun??o que tira todos os acentos e pontua??es de um vetor de strings.
+  # Par?metros:
+  # str - vetor de strings que ter?o seus acentos retirados.
+  # patterns - vetor de strings com um ou mais elementos indicando quais acentos dever?o ser retirados.
+  #            Para indicar quais acentos dever?o ser retirados, um vetor com os s?mbolos dever?o ser passados.
+  #            Exemplo: pattern = c("?", "^") retirar? os acentos agudos e circunflexos apenas.
+  #            Outras palavras aceitas: "all" (retira todos os acentos, que s?o "?", "`", "^", "~", "?", "?")
+  if(!is.character(str))
+    str <- as.character(str)
+  
+  pattern <- unique(pattern)
+  
+  if(any(pattern=="?"))
+    pattern[pattern=="?"] <- "?"
+  
+  symbols <- c(
+    acute = "????????????",
+    grave = "??????????",
+    circunflex = "??????????",
+    tilde = "??????",
+    umlaut = "???????????",
+    cedil = "??"
+  )
+  
+  nudeSymbols <- c(
+    acute = "aeiouAEIOUyY",
+    grave = "aeiouAEIOU",
+    circunflex = "aeiouAEIOU",
+    tilde = "aoAOnN",
+    umlaut = "aeiouAEIOUy",
+    cedil = "cC"
+  )
+  
+  accentTypes <- c("?","`","^","~","?","?")
+  
+  if(any(c("all","al","a","todos","t","to","tod","todo")%in%pattern)) # opcao retirar todos
+    return(chartr(paste(symbols, collapse=""), paste(nudeSymbols, collapse=""), str))
+  
+  for(i in which(accentTypes%in%pattern))
+    str <- chartr(symbols[i],nudeSymbols[i], str)
+  
+  return(str)
+}
